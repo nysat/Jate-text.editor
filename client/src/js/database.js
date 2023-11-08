ecf67b6d06882cf4fdf62d777a91c0c014b3bc05
nysat <nysa-cere3@hotmail.com>
2023-11-07 21:47:35 -0500

    changed the database file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,4 @@
 import { openDB } from 'idb';
-
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -11,38 +10,23 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
-
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-   console.log('PUT to database');
-   // Create a connection to the database database and version we want to use.
-   const contactDb = await openDB('jate', 1);
-   // Create a new transaction and specify the database and data privileges.
-   const tx = contactDb.transaction('jate', 'readwrite');
-   // Open up the desired object store.
-   const store = tx.objectStore('jate');
-   // Use the .add() method on the store and pass in the content.
-   const request = store.put({ id: 1, value: content });
-   // Get confirmation of the request.
-   const result = await request;
-   console.log('data saved to the database', result);
- };
-
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => { 
-  console.log('GET from database');
-  // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
-  // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readonly');
-  // Open up the desired object store.
+  const db = await initdb('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  // Use the .getAll() method on the store.
-  const request = store.getAll();
-  // Get confirmation of the request.
+  const request = store.put({id: 1, value: content});
   const result = await request;
-  console.log('data retrieved from the database', result);
-}
-
+  console.log('added item to the store os', result);
+};
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const allContent = store.getAll();
+  const result = await allContent;
+  console.log('result.value', result);
+  return result?.value;
+};
 initdb();
